3bd58c712c74982b59ac22a86d618d7dcde5684d
Maynor <vasquez158@gmail.com>
2026-04-30 18:22:17 +0000

    se agrega soporte para consumo de inventario

--- sap_integration/api/blueprint.py
+++ sap_integration/api/blueprint.py
@@ -5,6 +5,44 @@ from collections import defaultdict
 from .sap_auth import get_credentials
 
 
+def construir_url_sap(mapeo_lista: dict, empresa,endpoint, usa_paginacion=True, almacen = None,top: int = 20, skip: int = 0):
+    """
+    Construye la URL final a consumir desde SAP Service Layer usando el mapeo proporcionado.
+    """
+    if not mapeo_lista or "url" not in mapeo_lista:
+        raise ValueError("El mapeo no contiene una URL válida.")
+
+    try:
+        creds, url, endpoint_login = get_credentials(empresa)
+        base_url = f"{url.rstrip('/')}/{endpoint.lstrip('/')}"
+        campos = mapeo_lista.get("sap_fields", {}).get("head", {})
+        filtros = mapeo_lista.get("filters", [])
+        
+        params = []
+
+        if almacen:
+            filtros = [f"WhsCode eq '{almacen}'"]
+
+        if filtros:
+            filtro_sap = construir_filtro(filtros)
+            params.append(f"$filter={filtro_sap}")
+
+        if campos:
+            select_fields = ",".join(campos.values())
+            params.append(f"$select={select_fields}")
+
+        if usa_paginacion:
+            params.append(f"$top={top}")
+            params.append(f"$skip={skip}")
+
+        url_final = f"{base_url}?" + "&".join(params)
+        return url_final
+
+    except Exception as e:
+        frappe.log_error("Error en construcción de URL SAP", traceback.format_exc())
+        raise Exception(f"Error construyendo la URL SAP: {str(e)}")
+    
+
 def construir_filtro(lista_filtros):
     if not lista_filtros:
         return ""
@@ -26,42 +64,6 @@ def construir_filtro(lista_filtros):
             condiciones.append(f"({' or '.join(grupo)})")
 
     return " and ".join(condiciones)
-
-
-def construir_url_sap(mapeo_lista: dict, empresa,endpoint, usa_paginacion=True, almacen = None,top: int = 20, skip: int = 0):
-    """
-    Construye la URL final a consumir desde SAP Service Layer usando el mapeo proporcionado.
-    """
-    if not mapeo_lista or "url" not in mapeo_lista:
-        raise ValueError("El mapeo no contiene una URL válida.")
-
-    try:
-        creds, url, endpoint_login = get_credentials(empresa)
-        base_url = f"{url.rstrip('/')}/{endpoint.lstrip('/')}"
-        campos = mapeo_lista.get("sap_fields", {}).get("head", {})
-        filtros = mapeo_lista.get("filters", [])
-        
-        params = []
-
-        if filtros:
-            filtro_sap = construir_filtro(filtros)
-            params.append(f"$filter={filtro_sap}")
-
-        if campos:
-            select_fields = ",".join(campos.values())
-            params.append(f"$select={select_fields}")
-
-        if usa_paginacion:
-            params.append(f"$top={top}")
-            params.append(f"$skip={skip}")
-
-        url_final = f"{base_url}?" + "&".join(params)
-        return url_final
-
-    except Exception as e:
-        frappe.log_error("Error en construcción de URL SAP", traceback.format_exc())
-        raise Exception(f"Error construyendo la URL SAP: {str(e)}")
-    
 
 def mapping_blueprint1(doctype, key_field_sap, key_field_erpnext):
     """Obtiene el mapeo de campos desde un Doctype personalizado, organizando por niveles: head, DocumentLines y BatchNumbers."""
@@ -180,59 +182,3 @@ def obtener_mapeo(doctype_padre, doctype_hijo, campos_mapeo):
     except Exception as e:
         frappe.log_error(_("Error en obtener_mapeo"), f"Doctypes: {doctype_padre}/{doctype_hijo}\nError: {str(e)}")
         return {"success": False, "error": str(e)}
-
-
-# def mapping_blueprint(doctype, key_field_sap, key_field_erpnext):
-#     """Obtiene el mapeo de campos desde un Doctype personalizado, incluyendo URL y filtros opcionales."""
-#     resultado = obtener_mapeo(
-#         doctype_padre=doctype,
-#         doctype_hijo="Mapeo campos",
-#         campos_mapeo={
-#             "campo_erp": "campo_erpnext",
-#             "campo_externo": "campo_sap",
-#             "tipo": "tipo",          # puede ser 'map', 'url' o 'filter'
-#             "valor": "valor"         # usado para filtros y url
-#         }
-#     )
-
-#     if not resultado or not resultado.get('success') or not resultado.get('data'):
-#         frappe.throw("No se pudo obtener el mapeo de campos o la estructura es inválida.")
-
-#     mapeo = {
-#         "key_field": key_field_sap,
-#         "erp_key_field": key_field_erpnext,
-#         "sap_fields": {},
-#         "defaults": {},
-#         "url": None,
-#         "filters": []
-#     }
-
-#     for item in resultado["data"]:
-#         tipo = (item.get("tipo") or "").strip().lower()
-#         campo_erp = (item.get("campo_erpnext") or "").strip()
-#         campo_sap = (item.get("campo_sap") or "").strip()
-#         valor = (item.get("valor") or "").strip()
-
-#         if tipo == "map":
-#             if campo_erp and campo_sap:
-#                 mapeo["sap_fields"][campo_erp] = campo_sap
-#             continue
-
-#         elif tipo == "url" and valor:
-#             mapeo["url"] = valor
-
-#         elif tipo == "filter" and campo_sap and valor:
-#             valores = [v.strip() for v in valor.split(",") if v.strip()]
-#             if len(valores) > 1:
-#                 #filtro = f"{campo_sap} in ({', '.join([f'\'{v}\'' for v in valores])})"
-#                 valores_con_comillas = [f"'{v}'" for v in valores]
-#                 filtro = f"{campo_sap} in ({', '.join(valores_con_comillas)})"
-#             else:
-#                 filtro = f"{campo_sap} eq '{valores[0]}'"
-#             mapeo["filters"].append(filtro)
-
-#     # Si no hay campos mapeados pero sí hay URL, se permite continuar
-#     if not mapeo["sap_fields"] and not mapeo["url"]:
-#         frappe.throw("No se encontraron campos mapeados ni URL para consumir los datos.")
-
-#     return mapeo
--- sap_integration/api/sap_stock.py
+++ sap_integration/api/sap_stock.py
@@ -7,32 +7,51 @@ from sap_integration.utils.procesar_empresa_individual import procesar_empresa_i
 from sap_integration.api.last_update import sync_tracker, sync_tracker_update
 
 @frappe.whitelist()
+def sincronizar_lista_stock(docname=None, doctype_logs=None, almacen=None):
+    
 
-def sincronizar_lista_stock(docname=None, doctype_logs = None, almacen = None):
+    if not doctype_logs:
+        doctype_logs = "Sincronizacion Inventario SAP"
     usa_paginacion = True
-    doctype_logs = "Sincronizacion clientes SAP"
-    doctype_target =  "Customer"
-    doctype_mapeo = "Mapeo Cliente"
-    key_erpnext = "custom_cardcode"
-    key_sap = "CardCode"
-
+    doctype_target = "Stock Entry"
+    doctype_mapeo = "Mapeo Inventario SAP"
+    key_erpnext = "custom_itemcode"
+    key_sap = "ItemCode"
+    sap_whscode = None
     config = frappe.get_doc(doctype_mapeo, docname)
-
     resultados = []
+    empresas_a_procesar = []
 
-    for empresa in config.company_detalle:
-        resultado = procesar_empresa_individual(config, 
-                                                empresa,
-                                                docname,
-                                                procesar_datos,
-                                                doctype_logs,
-                                                doctype_target,
-                                                doctype_mapeo,
-                                                key_sap,
-                                                key_erpnext,
-                                                usa_paginacion,
-                                                almacen
-                                                )
+    # 🔹 Caso 1: viene almacen → solo una empresa
+    if almacen:
+        sap_whscode = frappe.db.get_value("Warehouse", almacen, "custom_warehousecode")
+        company = frappe.db.get_value("Warehouse", almacen, "company")
+
+        # buscar la empresa en el config
+        for empresa in config.company_detalle:
+            if empresa.company == company:
+                empresas_a_procesar.append(empresa)
+                break
+
+    # 🔹 Caso 2: no viene almacen → todas las empresas
+    else:
+        empresas_a_procesar = config.company_detalle
+
+    # 🔹 Ejecutar proceso
+    for empresa in empresas_a_procesar:
+        resultado = procesar_empresa_individual(
+            config,
+            empresa,
+            docname,
+            procesar_datos,
+            doctype_logs,
+            doctype_target,
+            doctype_mapeo,
+            key_sap,
+            key_erpnext,
+            usa_paginacion,
+            sap_whscode
+        )
 
         resultados.append({
             "company": empresa.company,
@@ -43,4 +62,4 @@ def sincronizar_lista_stock(docname=None, doctype_logs = None, almacen = None):
     return resultados
 
 def procesar_datos(registro_sap, mapeo_lista, doctype, company,debug_messages):
-    test = 1+1
+    print(f"valores registros sap: {registro_sap}")
