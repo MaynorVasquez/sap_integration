b40c9de4093c16aeffea8b0592cd800f39fd64bf
Maynor <vasquez158@gmail.com>
2026-05-13 16:33:26 +0000

    se agrega campo company en clientes y articulos

--- sap_integration/job/job_master.py
+++ sap_integration/job/job_master.py
@@ -2,12 +2,12 @@ import frappe
 from sap_integration.api import (
     sap_almacenes,
     sap_articulos_grupo,
-    sap_uom_categoria,
-    sap_uom,
-    sap_uom_conversion,
     sap_clientes_grupos,
     sap_lista_precio,
-    sap_vendedores
+    sap_vendedores,
+    sap_uom,
+    sap_uom_categoria,
+    sap_uom_conversion
 
 )
 
@@ -16,8 +16,9 @@ def job_secuenciales():
     ligeros = [
         ("Almacenes", sap_almacenes.sincronizar_lista_alamacenes),
         ("Grupo de articulos", sap_articulos_grupo.sincronizar_articulos_grupo),
-        ("Categoria UOM", sap_uom_categoria.sincronizar_uom_categoria),
         ("Lista UOM", sap_uom.sincronizar_uom),
+        ("Lista UOM", sap_uom.sincronizar_uom),
+        ("Categoria UOM", sap_uom_categoria.sincronizar_uom_categoria),
         ("Lista UOM Conversion", sap_uom_conversion.sincronizar_uom_conversion),
         ("Grupo cliente", sap_clientes_grupos.sincronizar_lista_clientes_grupos),
         ("Lista de precios", sap_lista_precio.sincronizar_lista_precio),
